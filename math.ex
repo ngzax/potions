edca3d2a5ed9c21170d487a67379e0da9e06ea3e
Daryl Richter <daryl@revzilla.com>
2014-12-17 14:39:28 -0500

    recursion & processes

--- math.ex
+++ math.ex
@@ -7,6 +7,24 @@ defmodule Math do
     a + b
   end
 
+  "Map"
+  def double_each([head|tail]) do
+    [head * 2| double_each(tail)]
+  end
+
+  def double_each([]) do
+    []
+  end
+
+  "Reduce"
+  def sum_list([head|tail], accumulator) do
+    sum_list(tail, head + accumulator)
+  end
+
+  def sum_list([], accumulator) do
+    accumulator
+  end
+
   def zero?(0) do
     true
   end
