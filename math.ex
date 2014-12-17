ca15cba419150c7586862bcb3270cd85a3a3179f
Daryl Richter <daryl@revzilla.com>
2014-12-17 11:06:43 -0500

    Initial Commit

---
+++ math.ex
@@ -0,0 +1,17 @@
+defmodule Math do
+  def sum(a, b) do
+    a + b
+  end
+
+  defp do_sum(a, b) do
+    a + b
+  end
+
+  def zero?(0) do
+    true
+  end
+
+  def zero?(x) when is_number(x) do
+    false
+  end
+end
---
+++ simple.exs
@@ -0,0 +1,5 @@
+#!/usr/bin/env elixir
+
+IO.puts "Hello world
+from Elixir"
+
