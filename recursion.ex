a19466da885a4c07c6f6d8245c61079011cc4b24
Daryl Richter <daryl@revzilla.com>
2016-03-14 12:17:20 -0400

    Add a refinement on remove dupes from Array.
    Based on this article: http://bit.ly/1RJAg2H

--- recursion.ex
+++ recursion.ex
@@ -7,5 +7,13 @@ defmodule Recursion do
     IO.puts msg
     print_multiple_times(msg, n - 1)
   end
+  
+  def rem_dupes([]), do: []
+  def rem_dupes(list), do: rem_dupes_sorted(Enum.sort(list))
+  def rem_dupes_sorted([first | t]), do: [first | rem_dupes_sorted(t, first)]
+  
+  def rem_dupes_sorted([], _), do: []
+  def rem_dupes_sorted([same | t], same), do: rem_dupes_sorted(t, same)
+  def rem_dupes_sorted([next | t], _last), do: [next | rem_dupes_sorted(t, next)]
+  
 end
-
